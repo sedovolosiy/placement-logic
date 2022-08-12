57fdfa57f1298a29b0727637434fe76f04128952
Serhii Siedovolosyi <sedovolosiy@gmail.com>
2022-08-13 00:51:27 +0200

    чистка стилей рубокоп. рефакторинг

--- predict.rb
+++ predict.rb
@@ -1,19 +1,19 @@
-require 'awesome_print'
+# frozen_string_literal: true
 
-# rubocop:disable Style/Documentation
+# Class for create layout of combinations
 class Predict
   PLATE_SIZES = {
     96 => { rows: 8, columns: 12 },
     384 => { rows: 16, columns: 24 }
   }.freeze
 
-  def initialize(plate_size = 96, samples = [], reagents = [], num_of_replicates = [])
+  def initialize(plate_size, samples, reagents, num_of_replicates)
     @plate_size = plate_size
     @samples = samples
     @reagents = reagents
     @num_of_replicates = num_of_replicates
-    @count_of_cells = count_of_cells
-    @count_of_plates = calc_of_plates
+    @count_of_cells = calc_cells
+    @count_of_plates = calc_plates
     @layout = generate_empty_layout
   end
 
@@ -21,20 +21,23 @@ class Predict
     validate_num_replicates!
 
     pre_result = grouped_by_reagent(expanded_combination_result_flatten)
-    visualize(alternative_layout(pre_result.values.flatten(1)))
+    fill_layout(pre_result.values.flatten(1))
   end
 
   private
 
-  def alternative_layout(flatten_list = [])
+  # filling layout
+  def fill_layout(flatten_list = [])
     @layout.each do |plate|
       plate.map do |rows|
-        rows.map! { |_item| flatten_list.shift }
+        rows.map! { flatten_list.shift }
+        break if flatten_list.empty?
       end
     end
     @layout
   end
 
+  # prepared flatted array for future group by reagent
   def expanded_combination_result_flatten
     @samples.map.with_index do |sample_list, index|
       sample_list.sort.map do |elem|
@@ -47,7 +50,8 @@ class Predict
     end.flatten(3).sort
   end
 
-  def count_of_cells
+  # calculate total count of cells for define count of plate
+  def calc_cells
     count = 0
     @samples.each_with_index do |sample, index|
       count += sample.size * @reagents[index].size * @num_of_replicates[index]
@@ -55,28 +59,28 @@ class Predict
     count
   end
 
-  def calc_of_plates
+  # total count of plates
+  def calc_plates
     (@count_of_cells / @plate_size) + ((@count_of_cells % @plate_size).zero? ? 0 : 1)
   end
 
+  # generating fixed size array for future data filling
   def generate_empty_layout
     @count_of_plates.times.each_with_object([]) do |_idx, out|
-      out << Array.new(PLATE_SIZES[@plate_size][:rows]) { Array.new(PLATE_SIZES[@plate_size][:columns], '-') }
+      out << Array.new(PLATE_SIZES[@plate_size][:rows]) { Array.new(PLATE_SIZES[@plate_size][:columns]) }
     end
   end
 
+  # simple validation
   def validate_num_replicates!
     return unless @samples.size != @num_of_replicates.size
 
-    raise StandardError, "Invalid data for generating layout #{@samples.size} != #{@num_of_replicates.size}"
+    raise ArgumentError, 'Invalid parameters'
   end
 
+  # group by reagents for next loop as groupped data
   def grouped_by_reagent(data)
     data.group_by { |item| item[1].itself }
-  end
-
-  def visualize(array)
-    ap array, multiline: false
   end
 end
 
@@ -84,4 +88,4 @@ prediction = Predict.new(96,
                          [%w[Sample-1 Sample-2 Sample-3], %w[Sample-1 Sample-2]],
                          [['<Pink>', '<Yellow>', '<Green>'], ['<Green>']],
                          [7, 21])
-puts prediction.result_layout
+p prediction.result_layout
